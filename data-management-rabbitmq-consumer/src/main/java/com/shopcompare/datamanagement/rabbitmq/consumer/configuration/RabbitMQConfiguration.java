32b3d9f648b121d50d12c1282006c8fef60cb44e
stefanikostic <stefanikostic6j@gmail.com>
2024-03-07 15:53:54 +0100

    Removed obsolete scanning of ElasticSearchClientIndexing and replaced amqp core wildcard import with direct imports.

--- data-management-rabbitmq-consumer/src/main/java/com/shopcompare/datamanagement/rabbitmq/consumer/configuration/RabbitMQConfiguration.java
+++ data-management-rabbitmq-consumer/src/main/java/com/shopcompare/datamanagement/rabbitmq/consumer/configuration/RabbitMQConfiguration.java
@@ -1,8 +1,11 @@
 package com.shopcompare.datamanagement.rabbitmq.consumer.configuration;
 
-import com.shopcompare.elasticsearch.client.indexing.ElasticSearchClientIndexing;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,7 +16,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-@ComponentScan(basePackageClasses = {ElasticSearchClientIndexing.class})
 public class RabbitMQConfiguration {
     private final RabbitMQProperties rabbitMQProperties;
 
