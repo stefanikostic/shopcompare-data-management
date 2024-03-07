d1cd1e4d778b392878e312c2e8eaaed840f37053
stefanikostic <stefanikostic6j@gmail.com>
2024-03-07 15:13:28 +0100

    Exposed endpoints for fetching products by category and shop name and shop name not.

---
+++ data-management-application/src/main/java/com/shopcompare/datamanagement/application/controller/ProductsAPI.java
@@ -0,0 +1,33 @@
+package com.shopcompare.datamanagement.application.controller;
+
+
+import com.shopcompare.datamanagement.business.service.ProductService;
+import com.shopcompare.datamanagement.persistence.domain.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductsAPI {
+
+    private final ProductService productService;
+
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProductsByCategoryAndShopName(@RequestParam("shopName") String shopName,
+                                                                          @RequestParam("categoryId") int categoryId) {
+        return ResponseEntity.ok(productService.getProductsByShopAndCategory(shopName, categoryId));
+    }
+
+    @GetMapping("/products/not-in-shop")
+    public ResponseEntity<List<Product>> getProductsByCategoryAndShopNameNot(@RequestParam("shopName") String shopName,
+                                                                          @RequestParam("categoryId") int categoryId) {
+        return ResponseEntity.ok(productService.getProductsCategoryAndShopNameNot(shopName, categoryId));
+    }
+
+}
